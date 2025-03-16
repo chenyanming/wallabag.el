a7c1b8627811ca00cbc1a90810420499404b9fb6
chenyanming <elecming@gmail.com>
2025-03-16 09:54:40 +0900

    Add LICENSE

--- wallabag-faces.el
+++ wallabag-faces.el
@@ -1,5 +1,27 @@
 ;;; wallabag-faces.el --- Emacs wallabag client faces -*- lexical-binding: t; -*-
 
+;; Author: Damon Chan <elecming@gmail.com>
+;; Maintainer: Damon Chan <elecming@gmail.com>
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
 (defface wallabag-current-match
   '((((class color) (background light))
      :background "#D8DEE9" :extend t)
